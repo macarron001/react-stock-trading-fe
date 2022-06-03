9eb12bbc8dca4a07881b239c29c632d7bfc99b46
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-03 21:45:31 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+
 
 function App() {
   return (
     <>
-        <div class="flex flex-row justify-center">
+        <div class="flex flex-row justify-end">
         <div class="basis-1/4">01</div>
-        <div class="basis-1/4">02</div>
+        <div class="basis-1/4"><h1>2</h1></div>
         <div class="basis-1/4">03</div>
       </div>
     </>
