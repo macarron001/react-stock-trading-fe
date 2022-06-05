12746549da785f994e7fafd1e9d3fa90e0eef912
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-05 23:16:02 +0800

    added and styled portfolio with fixes to be made later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar";
 import Register from "./components/Register"
 import Login from "./components/Login"
+import Portfolio from "./components/trader/Portfolio";
 
 function App() {
   return (
-    <Register />
+    <>
+      <Navbar />
+      <Portfolio />
+    </>
   )
 }
 
---
+++ src/components/trader/Portfolio.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+const Portfolio = () => {
+  const stocks = [
+    {
+      symbol : 'NVDA',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'BLH',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRW',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRA',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRB',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRC',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRD',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRE',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRF',
+      company_name : 'HEHE',
+      price : 2.00,
+    },  
+    {
+      symbol : 'GRG',
+      company_name : 'HEHE',
+      price : 2.00,
+    }
+  ]
+
+  return (
+    <div class="p-5 h-screen bg-gray-100">
+
+      <div class="overflow-auto rounded-lg shadow hidden md:block">
+      <table class="w-full">
+        <thead class="bg-gray-50 border-b-2 border-gray-200">
+          <tr>
+            <th class="w-60 p-3 text-sm font-semibold tracking-wide text-left">Stock</th>
+            <th class="w-60 p-3 text-sm font-semibold tracking-wide text-left">Company Name</th>
+            <th class="w-60 p-3 text-sm font-semibold tracking-wide text-left">Shares</th>
+            <th class="w-60 p-3 text-sm font-semibold tracking-wide text-left">Total</th>
+            <th class="w-10 p-3"></th>
+          </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-100">
+        {stocks !== "" && stocks.map((stock) => {
+          return (
+            <tr class="bg-white" key={stock.symbol}>
+            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
+            {stock.symbol}</td>
+            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
+            {stock.company_name}</td>
+            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
+            {stock.price}</td>
+            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
+            {stock.price}</td>  
+            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
+            <button className='border w-20 my-1 py-1 bg-green-600 hover:bg-emerald-300 text-white'>Sell Stock</button></td>
+          </tr>
+          )
+        })}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
