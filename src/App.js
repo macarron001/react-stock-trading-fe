366257a8eed46fd411c5ef30df6eb7d3583f822e
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-05 21:23:30 +0800

    added and styled login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navbar";
 import Register from "./components/Register"
+import Login from "./components/Login"
 
 function App() {
   return (
-    <Navbar />
+    <Login />
   )
 }
 
