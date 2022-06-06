470e1dc5584455f8508aab785cce62c277bfa013
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-06 12:58:06 +0800

    redesigned login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,24 +5,22 @@ import logoImg from '../stockx.png'
 
 export default function Login() {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-        <div className='hidden sm:block'>
-            <img className='w-full h-full object-cover sm:h-screen' src={login} alt="" />
-        </div>
+    <div className='relative h-screen w-full bg-zinc-900/90'>
+        <img className='absolute w-full h-full object-cover mix-blend-overlay sm:h-screen' src={login} alt="" />
 
-        <div className='bg-gray-100 flex flex-col justify-center'>
-            <form className='max-w-[400px] w-full mx-auto bg-white p-4'>
-            <img className='object-scale-down h-20 w-40 mx-auto' src={logoImg} alt="/" />
-                <div className='flex flex-col py-2'>
+        <div className='flex justify-center items-center h-full'>
+            <form className='max-w-[400px] w-full mx-auto bg-white p-8'>
+            <img className='object-scale-down h-20 w-40 mx-auto ' src={logoImg} alt="/" />
+                <div className='flex flex-col mb-4 font-semibold'>
                     <label>Email</label>
-                    <input className='border p-2' type="text" />
+                    <input className='border relative bg-gray-100 p-2' type="text" />
                 </div>
-                <div className='flex flex-col py-2'>
+                <div className='flex flex-col py-2 font-semibold'>
                     <label>Password</label>
-                    <input className='border p-2' type="password" />
+                    <input className='border relative bg-gray-100 p-2' type="password" />
                 </div>
-                <button className='border w-full my-5 py-2 bg-emerald-600 hover:bg-emerald-300 text-white'>Sign In</button>
-                <div className='flex justify-between'>
+                <button className='w-full py-3 mt-5 bg-emerald-500 hover:bg-emerald-500 relative text-white'>Sign In</button>
+                <div className='flex justify-between mt-3 font-medium'>
                     <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
                     <p>Create an account</p>
                 </div>
