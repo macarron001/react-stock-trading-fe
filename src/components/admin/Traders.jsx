dc4f879271542ff0678cf303f83f9e37972940e0
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-06 23:12:39 +0800

    added and styled current traders page

---
+++ src/components/admin/Traders.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+
+const Traders = () => {
+  const users = [
+    {
+      id: 1,
+      email : 'test1@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email : 'test2@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email : 'test3@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email : 'test4@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email : 'test5@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email : 'test6@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email :'test7@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email : 'test8@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },
+    {
+      id: 1,
+      email : 'test9@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    },  
+    {
+      id: 1,
+      email : 'test10@gmail.com',
+      balance: 1000,
+      created_at: '04/13/2009'
+    }
+  ]
+
+  return (
+    <div className="p-10 h-screen bg-gray-100">
+
+      <div className="overflow-auto rounded-lg shadow hidden md:block">
+      <table className="w-full">
+        <thead className="bg-gray-50 border-b-2 border-gray-200">
+          <tr>
+            <th className="w-25 p-3 text-sm font-semibold tracking-wide text-center">ID</th>
+            <th className="w-25 p-3 text-sm font-semibold tracking-wide text-center">Email</th>
+            <th className="w-25 p-3 text-sm font-semibold tracking-wide text-left">Created At</th>
+            <th className="w-25 p-3 text-sm font-semibold tracking-wide text-center">Balance</th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-100">
+        {users !== "" && users.map((user) => {
+          return (
+            <tr className="bg-white" key={user.email}>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap text-center">
+            {user.id}</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap text-center">
+            {user.email}</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">{user.created_at}</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap text-center">₱{user.balance}</td>
+          </tr>
+          )
+        })}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  )
+}
+
+export default Traders
